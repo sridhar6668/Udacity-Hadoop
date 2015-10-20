8d792254188e9898ef9487aaf148387a6090e7bc
training <training@localhost.localdomain>
2015-10-19 23:14:03 -0400

    Project Part 2 done

--- project/post_answer_length_relation/reducer.py
+++ project/post_answer_length_relation/reducer.py
@@ -3,14 +3,44 @@
 import sys
 
 oldKey = None
-hourArr = [0] * 24
-
+value = 0;
+count = 0;
 for line in sys.stdin:
         
 	data = line.strip().split("\t")
-	if(len(data) != 2):
+	if(len(data) != 3):
 		continue
+	
+	#thisKey, dataType, length = data;
+	if data[1] == "A":
+		
+		questionPostId = data[0]
+		questionLength = data[2]
 
+		if oldKey:
+			if count != 0:
+				print oldKey, "\t", oldQuestionnLength,  "\t", value/count
+			else:
+				print oldKey, "\t", oldQuestionnLength,  "\t", 0;
+		oldKey = questionPostId;
+		oldQuestionnLength = questionLength		
+		count = 0;
+		value = 0;
+
+	if data[1] == "B":
+		questionPostId = data[0]
+		answerLength = data[2]
+		value += int(answerLength)
+		count += 1
+		
+if oldKey:
+	if count != 0:
+		print oldKey, "\t", oldQuestionnLength,  "\t", value/count
+	else:
+		print oldKey, "\t", oldQuestionnLength,  "\t", 0;
+	
+			
+'''
 	thisKey, hour = data		
 	if oldKey and oldKey!=thisKey:
 		max = 0
@@ -36,7 +66,7 @@ if oldKey:
 	for i in range(0,24):
 		if(hourArr[i] == max and max > 0):
 			print oldKey, "\t", i+1
-				
+'''				
 	
 		
 	
